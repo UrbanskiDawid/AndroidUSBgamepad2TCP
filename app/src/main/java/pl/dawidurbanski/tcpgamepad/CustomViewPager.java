d769b34ea3b5c93abdedf9a4cda66dc3aafde081
Dawid Urbanski <github@dawidurbanski.pl>
2016-01-30 23:44:45 +0100

    CustomViewPager

---
+++ app/src/main/java/pl/dawidurbanski/tcpgamepad/CustomViewPager.java
@@ -0,0 +1,47 @@
+package pl.dawidurbanski.tcpgamepad;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by dawid on 30.01.2016.
+ */
+public class CustomViewPager extends ViewPager {
+
+    private boolean enabled = true;
+
+    private void init() {
+        this.enabled = true;
+    }
+
+    public CustomViewPager(Context context)
+    {
+        super(context);
+        init();
+    }
+    public CustomViewPager(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled)
+            return super.onTouchEvent(event);
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled)
+            return super.onInterceptTouchEvent(event);
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
